831a1e7d46db2c1ea22daae94e7e3ea4faaac034
Robin Norwood <robin.norwood@gmail.com>
2025-01-03 16:07:18 -0600

    Update collision code so it sorta works

--- scenes/poing/Scenes/ball.gd
+++ scenes/poing/Scenes/ball.gd
@@ -3,14 +3,37 @@ extends RigidBody2D
 class_name Ball
 
 const INITIAL_SPEED: float = 12000.0
+const BEYONCE: float = 500.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    # FIXME: I think there's a way to do this with signals that may be more efficient?
+    contact_monitor = true
+    max_contacts_reported = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+func _physics_process(delta: float) -> void:
+    # Works because contact_monitor and max_contacts_reported set in _ready()
+    var c: Array = get_colliding_bodies()
+
+    if c.size() and c[0] is Paddle:
+        var paddle: Paddle = c[0]
+        var dir: Vector2 = Vector2(global_position - paddle.global_position).normalized()
+        print("Bouncing off of " + str(paddle))
+        print("  Initial dir: " + str(dir))
+
+        # If we're headed "too far" up or down, clamp until we're under 45 degrees
+        # FIXME: I think the right way to do this is with cosines and stuff?
+        while abs(dir.y) > .5:
+            dir.y /= 2
+            dir = dir.normalized()
+
+        print("  Final dir: " + str(dir))
+
+        # This is "above and beyond" the normal physics interaction, I think?
+        apply_central_impulse(dir * BEYONCE)
 
 func start(loc: Vector2, dir: Vector2) -> void:
     transform.origin = loc
--- scenes/poing/Scenes/paddle.gd
+++ scenes/poing/Scenes/paddle.gd
@@ -2,8 +2,7 @@ extends CharacterBody2D
 
 class_name Paddle
 
-const SPEED = 30000.0
-const BEYONCE = 10.0
+const SPEED = 500.0
 
 var pos_action: String = ""
 var neg_action: String = ""
@@ -20,14 +19,21 @@ func _physics_process(delta: float) -> void:
     else:
         velocity.y = move_toward(velocity.y, 0, SPEED * delta)
 
-    move_and_slide()
+    var _collision = move_and_collide(velocity)
+    #if collision:
+        #print(collision)
+        #var collider = collision.get_collider()
+        #if collider is RigidBody2D:
+            #print(collider.linear_velocity)
 
-    for i in get_slide_collision_count():
-        var c = get_slide_collision(i)
-        var collider = c.get_collider()
-        if collider is RigidBody2D:
-            print(collider)
-            print(-c.get_normal())
-            print(collider.linear_velocity.length())
-            print(collider.mass)
-            collider.apply_central_impulse(-c.get_normal() * collider.linear_velocity.length() * collider.mass * BEYONCE) # Use ball velocity?
+    #for i in get_slide_collision_count():
+        #var c = get_slide_collision(i)
+        #var collider = c.get_collider()
+        #if collider is RigidBody2D:
+            #print(collider)
+            #print(c.get_normal())
+            #print(collider.linear_velocity.length())
+            #print(collider.mass)
+            #collider.apply_central_impulse(-c.get_normal() * collider.linear_velocity.length() * collider.mass * BEYONCE) # Use ball velocity?
+#
+            #collider.apply_central_impulse(-c.get_normal() * collider.linear_velocity.length() * collider.mass * BEYONCE) # Use ball velocity?
