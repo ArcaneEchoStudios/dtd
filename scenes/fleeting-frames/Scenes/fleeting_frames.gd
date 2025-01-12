da8d08173a8c319f61e459a3f42d5eebefb978af
Robin Norwood <robin.norwood@gmail.com>
2025-01-12 12:16:49 -0600

    Update to add round transition, timer, and score

--- scenes/fleeting-frames/Scenes/fleeting_frames.gd
+++ scenes/fleeting-frames/Scenes/fleeting_frames.gd
@@ -1,10 +1,14 @@
-extends Node2D
+extends CenterContainer
 
 class_name FleetingFrames
 
 @onready var button_container: GridContainer = get_node("UI/ButtonContainer")
 @onready var fruit_name_label: Label = get_node("UI/Prompt/FruitName")
+@onready var submit_button: Button = get_node("UI/Control/Submit")
+@onready var score_label: Label = get_node("UI/Control/RewardCoin/Score")
+@onready var round_timer_label: Label = get_node("UI/Control/RoundTimer")
 
+# Animations for checkmark, X, et. al.
 @export var sprite_animation: SpriteFrames
 
 const ButtonScene: PackedScene = preload("res://scenes/fleeting-frames/Scenes/button.tscn")
@@ -28,22 +32,56 @@ var FRUITS: Array =  [
     "Lymar"
 ]
 
+var SUBMIT_MESSAGES: Array =  [
+    "SUBMIT",
+    "APPLY",
+    "SEND",
+    "CONFIRM",
+    "COMPLETE",
+    "EXECUTE",
+    "UPLOAD",
+    "ENTER",
+    "ACCEPT",
+    "PROCEED",
+    "VALIDATE",
+    "FINALIZE",
+    "FORWARD",
+    "POST",
+    "DELIVER",
+    "APPROVE"
+]
+
 # FRUITS in random order per-game
 var fruits: Array = Array(FRUITS)
 var target_fruit: String
 
+# How far into the list of fruits do we delve for this round?
+# eg, start with just four choices, expand as the player "learns".
+var fruits_extent: int = 4
+
+var game_score: int = 0
 var round_score: int = 0
 
+var round_timer_running: bool = false
+var round_timer: float = 0.0
+var round_timer_factor: float = 1.0
+
 func _ready() -> void:
+    randomize()
     fruits.shuffle()
-    target_fruit = fruits.pick_random()
 
     start_round()
 
-func _process(_delta: float) -> void:
-    pass
+func _process(delta: float) -> void:
+    if round_timer_running:
+        round_timer -= delta * round_timer_factor
 
-func _on_button_pressed(button: Button) -> void:
+    if round_timer < 0:
+        end_round()
+
+    round_timer_label.text = "%1.2f" % [round_timer]
+
+func _on_fruit_button_pressed(button: Button) -> void:
     print(button.fruit_name)
 
     var success: bool = false
@@ -65,20 +103,64 @@ func _on_button_pressed(button: Button) -> void:
     update_score(success)
 
 func start_round() -> void:
+    print(fruits_extent)
+    # Each round, only give #fruits_extent options to choose from.
+    var current_round_fruits = fruits.slice(0, fruits_extent)
+
+    # Target fruit for this round.
+    target_fruit = current_round_fruits.pick_random()
+
+    round_score = 0
+
+    # Clear previous round buttons.
     for button in button_container.get_children():
         button_container.remove_child(button)
         button.queue_free()
 
+    # Populate current round buttons.
     for i in range(16):
         var button = ButtonScene.instantiate()
-        var loc: Vector2i = Vector2i(randi() % 4, randi() % 4)
-        var fruit: String = fruits[loc[0] * 4 | loc[1]]
+
+        var loc: Vector2i
+        var fruit: String
+
+        while fruit not in current_round_fruits:
+            # Calculate location on sprite sheet to select fruit from,
+            # then pick the name of the fruit in a way that's "stable" per game.
+            loc = Vector2i(randi() % 4, randi() % 4)
+            fruit = fruits[loc[0] * 4 | loc[1]]
+
+            # Now, if we picked a fruit that isn't in the current round, the while loop repeats.
+            #
+            # Throwing them out rather than picking from the current_round_fruits in the first place
+            # feels a bit jank, but this way I get a random picture/name relationship that's stable
+            # for each game, while only starting with a subset and expanding.
 
         button.configure(loc, fruit)
-        button.connect("pressed", Callable(self, "_on_button_pressed").bind(button))
+        button.connect("pressed", Callable(self, "_on_fruit_button_pressed").bind(button))
         button_container.add_child(button)
 
     fruit_name_label.text = target_fruit
+
+    # Configure SUBMIT button.
+    # 95% chance of first option ("SUBMIT"), otherwise a synonym.
+    if randf() < 0.95:
+        submit_button.text = SUBMIT_MESSAGES[0]
+    else:
+        submit_button.text = SUBMIT_MESSAGES.slice(1).pick_random()
+
+    score_label.text = str(game_score)
+
+    round_timer_running = true
+    round_timer = 5.0
+
+    # Each round, the timer runs up to 15% slower or faster. <3
+    round_timer_factor = 0.85 + randf() * (0.3)
+
+func end_round() -> void:
+    round_timer_running = false
+
+    start_round()
 
 func update_score(success: bool) -> void:
     # play sound
@@ -86,7 +168,7 @@ func update_score(success: bool) -> void:
     if success:
         round_score += 1
     else:
-        round_score = -1000 # One wrong move
+        round_score = -1000 # One wrong move...
 
 func _on_submit_pressed() -> void:
     var target_count: int = 0
@@ -96,5 +178,11 @@ func _on_submit_pressed() -> void:
 
     if target_count == round_score:
         print("Win")
+        game_score += 1
+
+        if fruits_extent < fruits.size():
+            fruits_extent += 1
     else:
         print("Loss")
+
+    end_round()
